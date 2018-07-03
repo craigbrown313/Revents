f8c7b9709a71f36fedf9e9d82860a1af2a667213
craigbrown313 <craigbrown313@gmail.com>
2018-07-03 15:52:26 -0400

    v1

---
+++ src/app/layouts/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { Button } from 'semantic-ui-react'
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+      <h1> Test</h1>
+      <button className="ui icon button">  <i className="smile icon"></i>
+      css button</button>
+      <Button/>
+      </div>
+    
+    );
+  }
+}
+
+export default App;
